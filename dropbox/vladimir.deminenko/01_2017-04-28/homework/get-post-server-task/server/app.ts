4ce24706361e9731ea9578b2e04017617950b2b9
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2017-05-03 19:02:01 +0200

    home work skeleton

--- dropbox/vladimir.deminenko/01_2017-04-28/homework/get-post-server-task/server/app.ts
+++ dropbox/vladimir.deminenko/01_2017-04-28/homework/get-post-server-task/server/app.ts
@@ -6,12 +6,46 @@
 import url = require("url");
 import fs = require("fs");
 import http = require("http");
+import mime = require("mime");
 
-const PORT = 3000;
+const PORT: number = 3000;
+const BAD_FILE_NAME_EXPRESSION: RegExp = new RegExp(/\/|^$|\.\./);
 
-var server = http.createServer(function (req, res) {
-    res.end('server was started and stopped#');
+var server = http.createServer((req, res) => {
+    let message: string = 'Bad Request';
+
+    if (~req.url.slice(1).search(BAD_FILE_NAME_EXPRESSION)) {
+        res.statusCode = 400;
+        res.end(message);
+
+        return;
+    }
+
+    const FILE_NAME: string[] = req.url.split('/').slice(-1);
+
+    switch (req.method) {
+        case 'GET': {
+            message = `get file: ${FILE_NAME}`;
+            break;
+        }
+        case 'POST': {
+            message = `post file: ${FILE_NAME}`;
+            break;
+        }
+        case 'DELETE': {
+            message = `delete file: ${FILE_NAME}`;
+            break;
+        }
+        default: {
+            res.statusCode = 400;
+        }
+
+    }
+
+    console.log(message);
+    res.end(message);
 });
 
-server.listen(PORT);
-console.log('server starts on port %s.', PORT);
+server.listen(PORT, () => {
+    console.log('server starts on port %s.', PORT);
+});
